b51f4a53ecf5e3a6e8578cfc0c7d543d2201bd62
aabine <kendoaustine@yahoo.com>
2023-05-17 06:29:10 +0100

    ALX

--- 1-shell.c
+++ 1-shell.c
@@ -5,129 +5,88 @@
 #include <string.h>
 
 char *find_command(char *command);
- char **tokenize(char *str);
+char **tokenize(char *str);
 
+int main() {
+    char input[1024];
+    char *path_env = getenv("PATH");
+    char **tokens;
+    char *path;
+    pid_t pid;
+    int status;
 
+    while (1) {
+        printf("cisfun$ ");
+        fgets(input, sizeof(input), stdin);
 
-int main()
-{
-	char command[1024];
-	char *paths = getenv("PATH");
-	char **tokens;
-	char *path;
-	pid_t pid;
-	int status;
+        input[strlen(input) - 1] = '\0';
 
-	while (1)
-	{
-		printf("cisfun$ ");
-		fgets(command, sizeof(command), stdin);
+        tokens = tokenize(input);
+        path = find_command(tokens[0]);
 
-		command[strlen(command) - 1] = '\0';
+        if (path == NULL) {
+            printf("command not found: %s\n", input);
+            continue;
+        }
 
-		tokens = tokenize(command);
-		path = find_command(tokens[0]);
+        pid = fork();
 
-		if (path == NULL)
-		{	
-			printf("commands not found: %s\n", command);
-			continue;
-		}
+        if (pid == -1) {
+            perror("fork failed");
+            return 1;
+        }
 
-		pid = fork();
+        if (pid == 0) {
+            execve(path, tokens, NULL);
+            perror(input);
+            exit(EXIT_FAILURE);
+        }
 
-		if (pid == - 1)
-		{
-			perror("fork failed");
-			return (1);
-		}
+        waitpid(pid, &status, 0);
+    }
 
-		else if (pid == 0)
-		{
-			execve(path, tokens, NULL);
-			perror(command);
-			exit(EXIT_FAILURE);
-		}
-
-		else
-		{
-			waitpid(pid, &status, 0);
-		}
-	}
-
-	return (0);
+    return 0;
 }
 
+char *find_command(char *command) {
+    char *path_env = getenv("PATH");
+    char *token;
+    char *path;
 
+    while ((token = strsep(&path_env, ":")) != NULL) {
+        path = malloc(strlen(token) + strlen(command) + 2);
 
+        if (path == NULL) {
+            perror("Memory allocation failed");
+            return NULL;
+        }
 
-char *find_command(char *command)
-{
-	char *paths = getenv("PATH");
-	char *token;
-	char *path;
+        sprintf(path, "%s/%s", token, command);
 
-	while ((token = strsep(&paths, ":")) != NULL)
-	{
-		path = malloc(strlen(token) + strlen(command) + 2);
+        if (access(path, F_OK) == 0) {
+            return path;
+        }
 
-		if (path == NULL)
-		{
-			perror("Memory allocation failed");
-			return (NULL);
-		}
+        free(path);
+    }
 
-		sprintf(path, "%s/%s", token, command);
-
-		if (access(path, F_OK) == 0)
-			return (path);
-	}
-	free(path);
-	return (NULL);
+    return NULL;
 }
 
+char **tokenize(char *str) {
+    char **tokens = NULL;
+    char *token = strtok(str, " ");
+    int i = 0;
 
-char **tokenize(char *str)
-{
-	char **tokens = NULL;
-	char *token = strtok(str, " ");
-	int i = 0;
+    while (token != NULL) {
+        tokens = realloc(tokens, sizeof(char *) * (i + 1));
+        tokens[i] = strdup(token);
+        i++;
+        token = strtok(NULL, " ");
+    }
 
-	while (token != NULL)
-	{
-		tokens = realloc(tokens, sizeof(char *) * (i + 1));
-		tokens[i] = strdup(token);
-		i++;
-		token = strtok(NULL, " ");
-	}
+    tokens = realloc(tokens, sizeof(char *) * (i + 1));
+    tokens[i] = NULL;
 
-	tokens = realloc(tokens, sizeof(char *) * (i + 1));
-	tokens[i] = NULL;
-
-	return (tokens);
+    return tokens;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
