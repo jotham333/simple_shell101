ed27676c725183ddcb5544450152984ba4be4d25
Jotham333 <jothamjosiah10@gmail.com>
2023-05-19 03:37:14 +0000

    ALX

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	char *input = NULL;
 	char *tokens[MAX_INPUT_SIZE];
-	char *tok = NULL;
+	char *path;
 	size_t len = 0;
 	char *token;
 	int i;
@@ -20,9 +20,8 @@ int main(void)
 		write(1, "cisfun$ ", 9);
 		_getline(&input, &len, stdin);
 		input[strcspn(input, "\n")] = 0;
-
+		
 		token = _strtok(input, " ");
-		tok = _strtok(NULL, " ");
 		i = 0;
 		while (token != NULL)
 		{
@@ -43,7 +42,8 @@ int main(void)
 		}
 		else if (_strcmp(tokens[0], "cd") == 0)
 		{
-			cd_builtin(tok);
+			path = tokens[1];
+			cd_builtin(path);
 		}
 		else
 		{
