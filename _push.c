4efe3230fe759a309793dd5660681f4c783e0e9a
aabine <kendoaustine@yahoo.com>
2023-06-23 04:57:08 +0100

    ALX

--- _push.c
+++ _push.c
@@ -1,51 +1,57 @@
 #include "monty.h"
 
-#include "monty.h"
-#include <stdlib.h>
-#include <ctype.h>
-
 /**
  * f_push - pushes an element onto the stack or queue
  * @stack: pointer to the head of the stack or queue
  * @line_number: current line number in the Monty program
  */
-void f_push(stack_t **stack, unsigned int line_number)
+void f_push(stack_t **head, unsigned int counter)
 {
-	char *endptr;
-	long int val;
-	stack_t *new;
+	int i, n;
+	stack_t *ptr;
 
-	if (globalVar.arg[1] == NULL)
+    if (globalVar.arg == NULL)
+    {
+        fprintf(stderr, "L%d: usage: push integer\n", counter);
+        fclose(globalVar.file);
+        free(globalVar.content);
+        free_stack(*head);
+        exit(EXIT_FAILURE);
+    }
+
+    i = 0;
+    if (globalVar.arg[i] == '-')
+        i++;
+    while (globalVar.arg[i] != '\0')
+    {
+        if (globalVar.arg[i] < '0' || globalVar.arg[i] > '9')
+        {
+            fprintf(stderr, "L%d: usage: push integer\n", counter);
+            fclose(globalVar.file);
+            free(globalVar.content);
+            free_stack(*head);
+            exit(EXIT_FAILURE);
+        }
+        i++;
+    }
+
+    n = atoi(globalVar.arg);
+    if (globalVar.is_stack == 0)
 	{
-		fprintf(stderr, "L%d: usage: push integer\n", line_number);
-		exit(EXIT_FAILURE);
+		ptr = malloc(sizeof(stack_t));
+		ptr->n = n;
+		ptr->next = *head;
+		*head = ptr;
+	}   
+    else
+	{
+        ptr = malloc(sizeof(stack_t));
+		ptr->n = n;
+		ptr->prev = NULL;
+		ptr->next = *head;
+		*head = ptr;
 	}
 
-	endptr;
-	val = strtol(globalVar.arg[1], &endptr, 10);
-
-	/* Check for errors in input */
-	if (*endptr != '\0' || endptr == globalVar.arg[1])
-	{
-		fprintf(stderr, "L%d: usage: push integer\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-
-	new = malloc(sizeof(stack_t));
-	if (new == NULL)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
-		exit(EXIT_FAILURE);
-	}
-
-	new->n = (int)val;
-	new->prev = NULL;
-	new->next = *stack;
-
-	if (*stack != NULL)
-		(*stack)->prev = new;
-
-	*stack = new;
-
-	printf("L%d: PUSH %d\n", line_number, new->n);
+    fclose(globalVar.file);
+    free(globalVar.content);
 }
